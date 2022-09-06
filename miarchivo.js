8804c6bd80ecacddbf17f8053715d14605f22d7a
sebaspalacios <Sebapalacio08@hotmail.com>
2022-09-06 16:42:40 -0300

    primer etapa de cambios

--- miarchivo.js
+++ miarchivo.js
@@ -1,4 +1,4 @@
-let y = 0
+/* let y = 0
  do {
     console.log(`Carpintero de rango ${y}`)
     y++
@@ -6,4 +6,28 @@ let y = 0
 
  for (let i = 0; i <=3; i++){
     alert (`Carpintero de rango ${i}`)
+ } */
+ 
+/*  function solicitaNombre () {
+   let nombre = prompt ("Ingrese su nombre uduario")
+   console.log (`El nombre del usuario es: ${nombre}`)
  }
+ */
+/*  link();
+ function link () {
+   let nombre = prompt ("Ingrese su nombre de usuario")
+   console.log (`El nombre del usuario es: ${nombre}`)
+ }   */
+
+const colores =["Rojo", "Verde", "Azul"]
+console.log (colores)
+
+colores.push ("Negro")
+console.log(colores)
+
+
+
+
+
+
+
